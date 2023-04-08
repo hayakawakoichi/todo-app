005c7411a932c347154df1910c9d89d42743849a
hayakawakoichi <koichi.hayakawa.dev@gmail.com>
2023-04-08 22:25:42 +0900

    add mantine

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,5 +1,17 @@
+import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+                fontFamily: 'Inter, sans-serif',
+                colorScheme: 'light',
+            }}
+        >
+            <Component {...pageProps} />
+        </MantineProvider>
+    )
 }
